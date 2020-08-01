96349ea894dddd132a8e5d3e3779624c7e49f2c8
Ivanuska82 <reynoldsivana82@gmail.com>
2020-08-01 12:45:47 -0400

    update HappyAgain

--- src/HappyAgain.java
+++ src/HappyAgain.java
@@ -2,6 +2,10 @@ public class HappyAgain {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("test again");
+        int a= 10;
+        int b= 20;
+        int sum = a+b;
+        System.out.println(sum);
 
 
         //all code is done here
